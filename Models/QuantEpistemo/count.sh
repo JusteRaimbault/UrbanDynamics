20bd3a6d2640411f662e3a99fb72ac637924f5d3
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-04-02 07:59:20 +0000

    count

---
+++ Models/QuantEpistemo/count.sh
@@ -0,0 +1,14 @@
+cd /home/ubuntu/ComplexSystems/UrbanDynamics/Models/QuantEpistemo 
+
+TOTAL=`mongo urbmod --quiet --eval "db.references.count()"`
+LINKS=`mongo urbmod --quiet --eval "db.links.count()"`
+REMAINING=`mongo urbmod --quiet --eval 'db.references.find({"citingFilled":false,"depth":{$gt:0}}).count()'`
+DATE=`date +%s`
+
+echo "$TOTAL;$LINKS;$REMAINING"
+
+echo "$DATE;$TOTAL;$LINKS;$REMAINING" >> progress.txt
+
+
+
+

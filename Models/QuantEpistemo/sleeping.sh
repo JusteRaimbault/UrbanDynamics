b179d5d53c71fe589253b16d2ff9cdcce9113b99
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-04-12 09:17:59 +0000

    .

--- Models/QuantEpistemo/sleeping.sh
+++ Models/QuantEpistemo/sleeping.sh
@@ -1,4 +1,11 @@
 
 # db.adminCommand({currentOp: true,"active" : false})["inprog"]
 
+# connections : db.serverStatus().connections
+
+# db.getSiblingDB("admin").aggregate( [    { $currentOp : { allUsers: true, idleSessions: true } },    { $match : { active: true } } ] )
+
+# list sessions
+# db.system.sessions.aggregate( [ { $listSessions: { users: [ {user: "myAppReader", db: "test" } ] } } ] ) on db config
+
 

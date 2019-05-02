4444df66fe6704d0375afe08d1d72510ac0ef08a
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-05-02 18:28:49 +0200

    submodulize_quantep

--- Models/QuantEpistemo/exportnetstat.sh
+++
@@ -1,5 +0,0 @@
-DATE=`date +'%Y%m%d_%H%M'`
-mongoexport -d urbmod -c netstat --type=csv -f "ipcount,docs" -q '{"ipcount":{$exists:true}}' -o netstat/netstat_$DATE.csv
-mongoexport -d urbmod -c netstat --type=csv -f "ip,success,ts" -q '{"ip":{$exists:true}}' -o netstat/netstat_all_$DATE.csv
-
-

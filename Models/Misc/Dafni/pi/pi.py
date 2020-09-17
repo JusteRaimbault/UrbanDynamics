f38c26151c407ff405e4ee09873826bdd7da5a8f
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-09-17 14:14:18 +0100

    Dafni: first tests

---
+++ Models/Misc/Dafni/pi/pi.py
@@ -0,0 +1,17 @@
+import math,os,random
+
+K = int(os.environ['STEPS'])
+B = int(os.environ['SAMPLES'])
+
+for k in range(K):
+    N=0
+    for i in range(B):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        if math.sqrt(x*x + y*y)<1: N = N+1
+    estimate = 4*N/B
+    print(estimate)
+
+    f = open('estimate.csv','a')
+    f.write(str(estimate)+"\n")
+    f.close()

8880a625014cb2ed0347a160f50e7087c6436268
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-09-21 16:42:52 +0100

    fix_dafni_model

--- Models/Misc/Dafni/pi/pi.py
+++ Models/Misc/Dafni/pi/pi.py
@@ -3,6 +3,9 @@ import math,os,random
 K = int(os.environ['STEPS'])
 B = int(os.environ['SAMPLES'])
 
+f = open('/data/outputs/estimate.csv','w')
+f.write("estimate\n")
+
 for k in range(K):
     N=0
     for i in range(B):
@@ -11,7 +14,6 @@ for k in range(K):
         if math.sqrt(x*x + y*y)<1: N = N+1
     estimate = 4*N/B
     print(estimate)
-
-    f = open('/data/outputs/estimate.csv','a')
     f.write(str(estimate)+"\n")
-    f.close()
+
+f.close()

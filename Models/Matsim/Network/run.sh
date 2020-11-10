091ab92957d145a8dafb16840791e6134aa86664
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-11-10 17:02:38 +0000

    dafni_matsim-network

--- Models/Matsim/Network/run.sh
+++ Models/Matsim/Network/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd spatialdata/library
-sbt "project spatialdata; runMain org.openmole.spatialdata.application.matsim.RunMatsim /root/test/road_line.shp;"
+sbt "project spatialdata; runMain org.openmole.spatialdata.application.matsim.RunMatsim --network --FUAName="$FUANAME" --FUAFile=/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg --datadir=/data/inputs/OSOpenRoads --output=/data/outputs/"$FUANAME"_Roads.xml;"
 #cp -r data /data/outputs

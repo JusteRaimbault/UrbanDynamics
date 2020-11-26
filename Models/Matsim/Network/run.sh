71b10615108b7d7b98922d0468fa1ad6c3f29b1f
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-11-26 21:52:21 +0000

    all_fuas_nw_process_matsim

--- Models/Matsim/Network/run.sh
+++ Models/Matsim/Network/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd spatialdata/library
-sbt "project spatialdata; runMain org.openmole.spatialdata.application.matsim.RunMatsim --network --FUAName="$FUANAME" --FUAFile=/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg --TilesFile=/data/OSOpenRoadsTiles.shp --datadir=/data/inputs/OSOpenRoads/data --output=/data/outputs/"$FUANAME"_Roads.xml;"
+sbt "project spatialdata; runMain org.openmole.spatialdata.application.matsim.RunMatsim --network --FUAName=\""$FUANAME"\" --FUAFile=/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg --TilesFile=/data/inputs/OSOpenRoadsTiles/OSOpenRoadsTiles.shp --datadir=/data/inputs/OSOpenRoads/data --output=/data/outputs/Network;"
 #cp -r data /data/outputs

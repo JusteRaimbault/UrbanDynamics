f484b6fa0984521fc2c2bec96d8b00135122e51f
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-11-30 16:41:31 +0000

    matsim_docker

--- Models/Matsim/Matsim/run.sh
+++ Models/Matsim/Matsim/run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# input files from upstream models
+cp /inputs/Network_$FUANAME.xml ./Network.xml
+cp /inputs/$FUANAME.xml ./Plans.xml
+
 rm -rf output
 java -Xmx2G -cp matsim-0.10.1/matsim-0.10.1.jar  org.matsim.run.Controler config.xml
 #cp /root/map_network.ipynb /data/outputs/map_network.ipynb
+mkdir -p /data/outputs
+cp -r output /data/outputs

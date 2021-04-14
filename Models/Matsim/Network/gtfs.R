5240e0892e00066589c52b62c8af20889a366807
Juste Raimbault <juste.raimbault@polytechnique.edu>
2021-04-14 09:14:32 +0100

    skeleton_slides_gisruk

--- Models/Matsim/Network/gtfs.R
+++ Models/Matsim/Network/gtfs.R
@@ -5,10 +5,21 @@ setwd(paste0(Sys.getenv('CS_HOME'),'/UrbanDynamics/Models/Matsim'))
 
 library(UK2GTFS)
 
-transxchange2gtfs(paste0(Sys.getenv('CS_HOME'),'/UrbanDynamics/Data/TNDS/data_20210322/SW.zip'))
+datadir = paste0(Sys.getenv('CS_HOME'),'/UrbanDynamics/Data/TNDS/data_20210322/')
+targetdir = paste0(Sys.getenv('CS_HOME'),'/UrbanDynamics/Data/TNDS/gtfs_20210322/')
 
-gtfs_write(
- gtfs = transxchange2gtfs(paste0(Sys.getenv('CS_HOME'),'/UrbanDynamics/Data/TNDS/data_20210322/EA.zip'),ncores=4),
- folder = paste0(Sys.getenv('CS_HOME'),'/UrbanDynamics/Data/TNDS/data_20210322'),
- name='EA_gtfs'
-)
+#transxchange2gtfs(paste0(datadir,'SW.zip'))
+
+#areas = c('EA','EM','L')
+# 'NCSD'
+areas = c('NE','NW','S','SE','SW','W','WM','Y')
+
+for(area in areas){
+  show(area)
+  gtfs_write(
+   gtfs = transxchange2gtfs(paste0(datadir,area,'.zip'),ncores=4),
+   folder = targetdir,
+   name=paste0(area,'_gtfs')
+  )
+}
+  

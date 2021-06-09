1253a142af96c5dffe8ae156a32b8acc4b245ff5
Juste Raimbault <juste.raimbault@polytechnique.edu>
2021-06-09 08:52:16 +0100

    add_pt2mmatsim_source

--- Models/Matsim/Network/run.sh
+++ Models/Matsim/Network/run.sh
@@ -18,15 +18,15 @@ if [ "$PARALLEL" = "true" ] ; then
     for NAME in ${FUANAME//;/ } ; do
         echo "run $NAME"
         sbt "project spatialdata; runMain org.openmole.spatialdata.application.matsim.RunMatsim --network --FUAName=\""$NAME"\" --FUAFile=/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg --TilesFile=/data/inputs/OSOpenRoadsTiles/OSOpenRoadsTiles.shp --datadir=/data/inputs/OSOpenRoads/data --output=/data/outputs/Network;" &
-        R -e 'source("functions.R"); extract_gtfs($NAME,/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg)'
+        #R -e 'source("functions.R"); extract_gtfs($NAME,/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg)'
     done
 else
     sbt "project spatialdata; runMain org.openmole.spatialdata.application.matsim.RunMatsim --network --FUAName=\""$FUANAME"\" --FUAFile=/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg --TilesFile=/data/inputs/OSOpenRoadsTiles/OSOpenRoadsTiles.shp --datadir=/data/inputs/OSOpenRoads/data --output=/data/outputs/Network;"
-    R -e 'source("functions.R"); extract_gtfs($FUANAME,/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg)'
+    #R -e 'source("functions.R"); extract_gtfs($FUANAME,/data/inputs/GHSFUAS/GHS_FUA_UCDB2015_GLOBE_R2019A_54009_1K_V1_0_WGS84.gpkg)'
 fi
 
 cp /root/map_network.ipynb /data/outputs/map_network.ipynb
-
+cp -r /root/gtfs /data/outputs/gtfs
 
 : '
 # test docker locally
